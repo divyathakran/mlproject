6a49bc7518496f23d110bbb21ac7690daebe790d
divyathakran <thakrandivya6@gmail.com>
2025-09-28 19:52:23 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -46,6 +46,16 @@ def evaluate_models(X_train, X_test, y_train, y_test, models,params):
 
             report[list(models.keys())[i]] = test_model_score
 
-        return report        
+        return report    
+        
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
