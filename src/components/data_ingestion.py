2f35f6a44ecefd5c9cba449be8d4179381a86d9d
divyathakran <thakrandivya6@gmail.com>
2025-09-28 12:17:01 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+#just to check everything is working fine
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str= os.path.join('artifacts',"train.csv")
@@ -22,7 +26,7 @@ class DataIngestion:
         def initiate_data_ingestion(self):
               logging.info("entered the data ingestion method or component")
               try:
-                    df= pd.read_csv('notebook\data\stud.csv') # you can read data from anywhere - ex - mongodb client
+                    df= pd.read_csv('notebook/data/stud.csv') # you can read data from anywhere - ex - mongodb client
                     logging.info('read the dataset as dataframe')
 
                     os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -48,4 +52,7 @@ class DataIngestion:
                     raise CustomException(e,sys)
 if __name__ =="__main__":
       obj=DataIngestion()
-      obj.initiate_data_ingestion()              
+      train_data,test_data=obj.initiate_data_ingestion()              
+
+      data_transformation = DataTransformation()
+      data_transformation.initiate_data_transformation(train_data,test_data)
